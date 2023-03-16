a632e611ed3d6b427bdf3773e058d1e63b068042
Tanya Pina <tanyapina15@gmail.com>
2023-03-15 22:33:21 -0700

    set event state and title form

--- client/src/components/formevent.js
+++ client/src/components/formevent.js
@@ -1,9 +1,19 @@
 import { useState } from "react";
 
+const [event, setEvent] = useState({title: "", location:"", eventtime: ""})
+
 const FormEvent = () => {
     return (
         <form>
-
+            <label>Title</label>
+            <input
+            type="text"
+            id="add-event-title"
+            placeholder="The Title of Your Event"
+            required
+            value={event.title} //state
+            onChange={handleTitleChange}
+            />
         </form>
     )
 }
