0e952de3218aa8a12be38ae5ba8ab0cc18034778
Tanya Pina <tanyapina15@gmail.com>
2023-03-15 23:49:13 -0700

    added handleLocationChange function

--- client/src/components/formevent.js
+++ client/src/components/formevent.js
@@ -1,23 +1,41 @@
 import { useState } from "react";
 
-const handleTitleChange = (e) => {
-    e.preventDefault();
-    let newTitle = e.target.value;
-    console.log(newTitle);
-}
-
 const FormEvent = () => {
-    const [event, setEvent] = useState({title: "", location:"", eventtime: ""})
+    const [event, setEvent] = useState({ title: "", location: "", eventtime: "" })
+
+    const handleTitleChange = (e) => {
+        e.preventDefault();
+        let newTitle = e.target.value;
+        setEvent((event) => ({...event, title: newTitle}));
+        console.log(event.title);
+    }
+    
+    const handleLocationChange = (e) => {
+        e.preventDefault();
+        let newLocation = e.target.value;
+        setEvent((event) => ({...event, location: newLocation}));
+        console.log(event.location);
+    }
+    
     return (
         <form>
             <label>Title</label>
             <input
-            type="text"
-            id="add-event-title"
-            placeholder="The Title of Your Event"
-            required
-            value={event.title} //state
-            onChange={handleTitleChange}
+                type="text"
+                id="add-event-title"
+                placeholder="The Title of Your Event"
+                required
+                value={event.title} //state
+                onChange={handleTitleChange}
+            />
+            <label>Location</label>
+            <input
+                type="text"
+                id="add-event-location"
+                placeholder="The Location of Your Event"
+                required
+                value={event.location} //state
+                onChange={handleLocationChange}
             />
         </form>
     )
